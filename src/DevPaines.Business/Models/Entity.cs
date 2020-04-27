cde169e1246cc7cd3f69dbe0838587997ca97c74
Leonardo <leonardopaines@hotmail.com>
2020-04-26 23:27:38 -0300

    Add project files.

---
+++ src/DevPaines.Business/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcBasica.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity() => this.Id = Guid.NewGuid();
+    }
+}
